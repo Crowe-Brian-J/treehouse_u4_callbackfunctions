9912837f7b50aa0be2a3fe7ae9b3f271ec6a899e
Brian Crowe <brian.j.crowe@protonmail.com>
2025-08-05 21:27:19 -0400

    function declaration to expression to anonymous to inlining

--- callback.js
+++ callback.js
@@ -1,7 +1,18 @@
-function sayHello() {
+//function declaration
+/* function sayHello() {
+  console.log('Hello')
+} */
+
+//function declaration -> function expression
+/* const sayHello = function () {
+  console.log('Hello')
+} */
+
+//function expression -> anonymous function
+/* function () {
   console.log('Hello')
 }
-
+ */
 //callback function not invoked
 const executeCallback = (callback) => {
   //callback function invoked ()
@@ -9,4 +20,9 @@ const executeCallback = (callback) => {
 }
 
 //when calling callback function, it is not invoked in call - no ()
-executeCallback(sayHello)
+/* executeCallback(sayHello) */
+
+//callback as anonymous function - also known as inlining
+executeCallback(function () {
+  console.log('Hello')
+})
